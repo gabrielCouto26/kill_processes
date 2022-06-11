66e16a48ef6e7825b333eb3c2c1a5461d1c34e68
Gabriel Couto <gabrielcouto26@gmail.com>
2022-06-11 04:30:46 -0300

    versao com tempfile

--- killer.rb
+++ killer.rb
@@ -1,22 +1,20 @@
-program_name = ARGV[0]
-tmp_file = "grep_output"
+require 'tempfile'
 
-system("ps aux | grep #{program_name} | awk '{print $2, $11}' >> #{tmp_file}")
+program_name = ARGV[0]
 
 begin
-  File.open(tmp_file) do |file|
+  Tempfile.open do |file|
+    system "ps aux | grep #{program_name} | awk '{print $2, $11}' >> #{file.path}"
     file.each do |line|
       next if /grep|ruby|sh/.match?(line)
       pid = line.match /^\d{1,6}/
       @response = system "kill -9 #{pid}"
     end
+    puts @response.nil? ? 
+      "Proccess #{program_name} is not running." :
+      "Proccess #{program_name} killed."
   end
-  puts @response.nil? ? 
-    "Proccess #{program_name} nonexistent." :
-    "Proccess #{program_name} killed."
 rescue StandardError => e
   raise "Failed in killing #{program_name}."
   raise e
-ensure
-  system "rm #{tmp_file}"
 end
