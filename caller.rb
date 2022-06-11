aa04decd177bd55bae99ff3613128f71c8cfdb5b
Gabriel Couto <gabrielcouto26@gmail.com>
2022-06-11 13:45:32 -0300

    separate roles

---
+++ caller.rb
@@ -0,0 +1,4 @@
+require './killer'
+
+proccess_name = ARGV[0]
+Killer.new(proccess_name).kill
--- killer.rb
+++ killer.rb
@@ -1,32 +1,32 @@
 require 'tempfile'
 
 class Killer
-  @program_name = ARGV[0]
+  def initialize(proccess_name)
+    @proccess_name = proccess_name
+  end
 
-  def self.kill
+  def kill
     Tempfile.open do |file|
-      system "ps aux | grep #{@program_name} | awk '{print $2, $11}' >> #{file.path}"
+      system "ps aux | grep #{@proccess_name} | awk '{print $2, $11}' >> #{file.path}"
       file.each do |line|
-        next if /grep|ruby|sh/.match?(line)
+        next if /grep|ruby|sh/.match? line
         pid = line.match /^\d{1,6}/
         @response = system "kill -9 #{pid}"
       end
       response
     end
   rescue StandardError => e
-    raise "Failed in killing #{@program_name}."
+    raise "Failed in killing #{@proccess_name}."
     raise e
   end
 
   private
 
-  def self.response
+  def response
     if @response.nil?
-      puts "Proccess #{@program_name} is not running."
+      puts "Proccess #{@proccess_name} is not running."
     else
-      puts "Proccess #{@program_name} killed."
+      puts "Proccess #{@proccess_name} killed."
     end
   end
 end
-
-Killer.kill
