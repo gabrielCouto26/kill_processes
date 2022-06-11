64374a0fd3502af135fb29d648460fc3a5cb7567
Gabriel Couto <gabrielcouto26@gmail.com>
2022-06-11 13:34:28 -0300

    implementa classe

--- killer.rb
+++ killer.rb
@@ -1,20 +1,32 @@
 require 'tempfile'
 
-program_name = ARGV[0]
+class Killer
+  @program_name = ARGV[0]
 
-begin
-  Tempfile.open do |file|
-    system "ps aux | grep #{program_name} | awk '{print $2, $11}' >> #{file.path}"
-    file.each do |line|
-      next if /grep|ruby|sh/.match?(line)
-      pid = line.match /^\d{1,6}/
-      @response = system "kill -9 #{pid}"
+  def self.kill
+    Tempfile.open do |file|
+      system "ps aux | grep #{@program_name} | awk '{print $2, $11}' >> #{file.path}"
+      file.each do |line|
+        next if /grep|ruby|sh/.match?(line)
+        pid = line.match /^\d{1,6}/
+        @response = system "kill -9 #{pid}"
+      end
+      response
     end
-    puts @response.nil? ? 
-      "Proccess #{program_name} is not running." :
-      "Proccess #{program_name} killed."
+  rescue StandardError => e
+    raise "Failed in killing #{@program_name}."
+    raise e
   end
-rescue StandardError => e
-  raise "Failed in killing #{program_name}."
-  raise e
+
+  private
+
+  def self.response
+    if @response.nil?
+      puts "Proccess #{@program_name} is not running."
+    else
+      puts "Proccess #{@program_name} killed."
+    end
+  end
 end
+
+Killer.kill
